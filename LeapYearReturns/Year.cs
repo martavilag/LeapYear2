4dda299ddb04a414769439a19be46d855016ec71
mvila <mvila@voxelgroup.net>
2021-11-30 15:36:09 +0100

    refactor: generalized solution production code

--- LeapYearReturns/Year.cs
+++ LeapYearReturns/Year.cs
@@ -11,13 +11,13 @@
 
         public bool IsLeap()
         {
-            if (year % 400 == 0)
+           if (year % 400 == 0)
                 return true;
-            if (year % 100 == 0 && year % 4 == 0)
-                return false;
-            if (year % 4 == 0)
-                return true;
-            return false;
+           if (year % 4 == 0 && year % 100 == 0)
+               return false;
+           if (year % 4 == 0)
+               return true;
+           return false;
         }
     }
 }

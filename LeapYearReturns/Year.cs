27c22d1519b881119116d5bddd49997fd9a9b69f
mvila <mvila@voxelgroup.net>
2021-11-30 15:37:09 +0100

    refactor: generalized a bit more the solution

--- LeapYearReturns/Year.cs
+++ LeapYearReturns/Year.cs
@@ -13,9 +13,7 @@
         {
            if (year % 400 == 0)
                 return true;
-           if (year % 4 == 0 && year % 100 == 0)
-               return false;
-           if (year % 4 == 0)
+           if (year % 4 == 0 && year % 100 != 0)
                return true;
            return false;
         }

928182ce4cc908386a6cbbfd5911c24b5c76eb7a
mvila <mvila@voxelgroup.net>
2021-11-30 14:44:14 +0100

    refactor: return true for years divisible by 4

--- LeapYearReturns/Year.cs
+++ LeapYearReturns/Year.cs
@@ -13,7 +13,7 @@
         {
             if (year % 400 == 0)
                 return true;
-            if (year == 2012 || year == 2036 || year == 1996)
+            if (year % 4 == 0)
                 return true;
             return false;
         }

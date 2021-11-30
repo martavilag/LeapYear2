be2af825c50fec6447771ed9768dd37dfb2641fd
mvila <mvila@voxelgroup.net>
2021-11-30 13:55:08 +0100

    test: return true for two thousand

--- LeapYearReturns.Tests/LeapYearShould.cs
+++ LeapYearReturns.Tests/LeapYearShould.cs
@@ -1,11 +1,16 @@
+using LeapYearReturns;
 using Xunit;
 
-namespace LeapYearReturns.Tests;
-
-public class LeapYearShould
+namespace LeapYearReturns.Tests
 {
-    [Fact]
-    public void Test1()
+    public class LeapYearShould
     {
+        [Fact]
+        public void ReturnTrueForTwoThousand()
+        {
+            Year year = new Year(2000);
+            Assert.True(year.IsLeap());
+        }
     }
+    
 }
---
+++ LeapYearReturns/Year.cs
@@ -0,0 +1,15 @@
+﻿namespace LeapYearReturns
+{
+    public class Year
+    {
+        public Year(int year)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsLeap()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
